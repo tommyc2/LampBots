0a428dfb45f7fbfe74f501158330870ec9ba486a
Igor Kapusniak <114166214+igorkapusniak0@users.noreply.github.com>
2024-02-22 09:12:15 +0000

    Create senderForTwoLamps.py

---
+++ senderForTwoLamps.py
@@ -0,0 +1,23 @@
+radio.set_group(23)
+serial.redirect(SerialPin.USB_TX, SerialPin.USB_RX, BaudRate.BAUD_RATE115200)
+
+def on_forever():
+    list2 = serial.read_line().split(",")
+    x = Math.round(parse_float(list2[0]))
+    y = Math.round(parse_float(list2[1]))
+    z = Math.round(parse_float(list2[2]))
+    w = Math.round(parse_float(list2[3]))
+    radio.send_value("x", x)
+    radio.send_value("y", y)
+    radio.send_value("z", z)
+    radio.send_value("w", w)
+
+    x1 = Math.round(parse_float(list2[4]))
+    y1 = Math.round(parse_float(list2[5]))
+    z1 = Math.round(parse_float(list2[6]))
+    w1 = Math.round(parse_float(list2[7]))
+    radio.send_value("x1", x1)
+    radio.send_value("y1", y1)
+    radio.send_value("z1", z1)
+    radio.send_value("w1", w1)
+basic.forever(on_forever)
